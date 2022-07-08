f8c4f75af14c88c7e90e6a036f05409858470aab
Fatih Kabay <mfthkabay@gmail.com>
2022-07-08 21:53:00 +0300

    continue project

--- myfirst-react-app/src/App.js
+++ myfirst-react-app/src/App.js
@@ -5,19 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+       
+        <p style={{ fontSize: 20 }}>
+          Bu proje 7 temmuz 2022 tarihinde react ile yapilmaya baslanmistir.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="div">Daha fazla react hakkinda bilgi icin aderesini takip edebilirsiniz.</div>
       </header>
+
     </div>
   );
 }
