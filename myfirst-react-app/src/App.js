35622d5fb0f42957e87aa95fc860190b3fac35f1
Fatih Kabay <mfthkabay@gmail.com>
2022-07-09 19:02:09 +0300

    continue project

--- myfirst-react-app/src/App.js
+++ myfirst-react-app/src/App.js
@@ -2,16 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 const firstName = 'Mehmet Fatih';
 const lastName = 'Kabay';
+const element = 'You have been hired'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
 
         <p style={{ fontSize: 20 }}>
-          Bu proje 9 temmuz 2022 tarihinde react ile yapilmaya baslanmistir.
+        This project was started on 9 July 2022 with React.
         </p>
-        <div className="div">Daha fazla react hakkinda bilgi icin aderesini takip edebilirsiniz.</div>
-      <h1>I'm, {firstName} {lastName}</h1></header>
+       
+      <h2>Hello, {firstName} {lastName} {element}</h2></header>
       
 <h1>
  
