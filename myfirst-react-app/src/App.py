7175a6cdeae8a4eabac77c5e1890fe6d9b1bf256
Fatih Kabay <mfthkabay@gmail.com>
2022-09-06 12:56:03 +0300

    initial commit

--- myfirst-react-app/src/App.py
+++
@@ -1,8 +0,0 @@
-ad_soyad=input("Adýnýz-Soyadýnýz: ")
-yabanci_dil=input("Bildiðiniz yabancý dil: ")
-yas=int(input("Yaþýnýz: "))
- 
-if ((yabanci_dil=="Ýngilizce" or yabanci_dil=="Fransýzca" or yabanci_dil=="Ýtalyanca" and yabanci_dil=="Türkçe") and yas<50):
-    print("Kriterlere uyduðunuz için iþe alýnýdnýz")
-else:
-    print("Kriterlere uyamadýðýnýz için iþe alýnmadýnýz")
